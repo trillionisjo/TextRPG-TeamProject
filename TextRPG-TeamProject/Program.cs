322d4df93e9f7b2699f52240b139b75e4e60e796
trillionisjo <trillionisjo@gmail.com>
2024-09-26 19:24:02 +0900

    Add scene

--- TextRPG-TeamProject/Program.cs
+++ TextRPG-TeamProject/Program.cs
@@ -1,6 +1,20 @@
 ﻿
-public class Program {
-    static void Main (string[] args) {
-        Console.WriteLine("Hello, World!");
+public class Program
+{
+    static void Main (string[] args)
+    {
+        Scene currentScene = new StartScene();
+        Scene nextScene = null;
+        
+        while (true)
+        {
+            if (currentScene != nextScene && nextScene != null)
+            {
+                currentScene = nextScene;
+                currentScene.Start();
+            }
+            currentScene.Update();
+            nextScene = currentScene.GetNextScene();
+        }
     }
 }
