6f96d9d5f29c260f09d0da788dd3185dbf6f8f4c
devH <146154052+0umj@users.noreply.github.com>
2024-09-30 11:27:55 +0900

    feat(creature,player): add MP variable to player class
    The Player class now has an MP variable to manage mana points.
    This change includes the following:
    - Added MP property to Player
    - Updated Player's setup method to include MP parameter

--- TextRPG-TeamProject/Scenes/StartScene.cs
+++ TextRPG-TeamProject/Scenes/StartScene.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using TextRPG_TeamProject.Scenes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 class StartScene : Scene
 {
@@ -7,28 +9,29 @@ class StartScene : Scene
         Console.Clear();
     }
 
+
     public override void Update ()
     {
         Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
         Console.WriteLine("이제 전투를 시작할 수 있습니다.");
-
-        string []option = {"상태 보기","전투 시작"};
-        UIManager.DisplaySelectionUI(option);
+        HandleInput();
     }
+
 
     private void HandleInput()
     {
-        string input = Console.ReadLine() ?? string.Empty;
-        int.TryParse(input, out var number);
+        string[] option = { "상태 보기", "전투 시작" };
+        int number = UIManager.DisplaySelectionUI(option);
 
         switch (number)
         {
-        case 1:
-            NextScene = new PlayerState();
-            break;
+            case 1:
+                NextScene = new PlayerState();
+                break;
 
-        default:
-            break;
+            default:
+                NextScene = new DungeonScene();
+                break;
         }
     }
 
