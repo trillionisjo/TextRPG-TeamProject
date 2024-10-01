e89c3eba85152b97485e0fd6d882533133df5b63
chijan0427 <wlsnd0427@gmail.com>
2024-10-01 16:52:59 +0900

    Update StartScene.cs

--- TextRPG-TeamProject/Scenes/StartScene.cs
+++ TextRPG-TeamProject/Scenes/StartScene.cs
@@ -15,7 +15,7 @@ class StartScene : Scene
         Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
         Console.WriteLine("이제 전투를 시작할 수 있습니다.");
 
-        string[] option = { "상태 보기", "전투 시작" };
+        string[] option = { "상태 보기", "전투 시작", "인벤토리" };
         int number = UIManager.DisplaySelectionUI(option);
         HandleInput(number);
     }
@@ -32,6 +32,9 @@ class StartScene : Scene
             break;
         case 2:
             NextScene = new DungeonScene();
+            break;
+        case 3:
+            NextScene = new InventoryScene();
             break;
         default:
             break;
