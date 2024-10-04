a6328ad768a645a770581aff9a01ec78ef96c63b
trillionisjo <trillionisjo@gmail.com>
2024-10-04 10:04:03 +0900

    Modify CollectionGalleryScene class
    - 항목을 바꾸면 바로 그림이 나오도록 수정.

--- TextRPG-TeamProject/Scenes/CollectionGalleryScene.cs
+++ TextRPG-TeamProject/Scenes/CollectionGalleryScene.cs
@@ -22,53 +22,120 @@ class CollectionGalleryScene : Scene
         }
     }
 
-    Point[] pos;
+    private struct Option
+    {
+        public string text;
+        public Action <int, int, Card> draw;
+    }
+
     int n;
+    Point[] drawPos;
+    string[] cardNameList;
+
     int cursor;
 
     public override void Start ()
     {
-        //CollectionData.Cards[(int)CardType.Mountain].Found = true;
-        //CollectionData.Cards[(int)CardType.Cherry].Found = true;
-        //CollectionData.Cards[(int)CardType.Fairy].Found = true;
-        //CollectionData.Cards[(int)CardType.CatInterruption].Found = true;
+        CollectionData.Cards[(int)CardType.Mountain].Found = true;
+        CollectionData.Cards[(int)CardType.Cherry].Found = true;
+        CollectionData.Cards[(int)CardType.Fairy].Found = true;
+        CollectionData.Cards[(int)CardType.CatInterruption].Found = true;
 
         n = CollectionData.Cards.Length;
-        pos = new Point[n];
-        pos[(int)CardType.Mountain] = new Point(20, 2);
-        pos[(int)CardType.Cherry] = new Point(20, 0);
-        pos[(int)CardType.Fairy] = new Point(20, 0);
-        pos[(int)CardType.CatInterruption] = new Point(20, 0);
-        pos[(int)CardType.Card5] = new Point(0, 0);
-        pos[(int)CardType.Card6] = new Point(0, 0);
-        cursor = 0;
-    }
-
-    public override void Update ()
-    {
-        Console.Clear();
-        Console.WriteLine("[수집 카드 목록]");
-        WriteCardList();
-    }
-
-    private void WriteCardList ()
-    {
-        Option[] options = new Option[n + 2];
+        cardNameList = new string[n];
 
         for (int i = 0; i < n; i++)
         {
             Card card = CollectionData.Cards[i];
             if (card.Found)
-                options[i] = new Option(card.Name, new CardSelectionEvent(pos[i].x, pos[i].y, card.AsciiArt));
+                cardNameList[i] = card.Name;
             else
-                options[i] = new Option("???", new CardSelectionEvent(0, 0, null));
+                cardNameList[i] = "???";
         }
 
-        options[n + 1] = new Option("나가기", new NextSceneEvent(this, new StartScene()));
+        drawPos = new Point[n];
+        drawPos[(int)CardType.Mountain] = new Point(30, 2);
+        drawPos[(int)CardType.Cherry] = new Point(30, 4);
+        drawPos[(int)CardType.Fairy] = new Point(30, 4);
+        drawPos[(int)CardType.CatInterruption] = new Point(30, 0);
+        drawPos[(int)CardType.Card5] = new Point(0, 0);
+        drawPos[(int)CardType.Card6] = new Point(0, 0);
 
-        (int x, int y) pt = Console.GetCursorPosition();
-        cursor = UIManager.DisplaySelectionUI(options, pt.x, pt.y, cursor);
+        cursor = 0;
+
+        Console.Clear();
     }
 
-    
+    public override void Update ()
+    {
+        Console.SetCursorPosition(0, 0);
+        Console.WriteLine("[수집 카드 목록]");
+        WriteCardList();
+        DrawArt();
+        HandleKeyInput();
+        Thread.Sleep(10);
+    }
+
+    private void WriteCardList ()
+    {
+        for (int i = 0; i < n; i++)
+        {
+            string str;
+            if (cursor == i)
+                str = $">> {cardNameList[i]}";
+            else
+                str = $"   {cardNameList[i]}";
+            Console.WriteLine(str);
+        }
+    }
+
+    private void HandleKeyInput()
+    {
+        if (!Console.KeyAvailable)
+            return;
+
+        var key = Console.ReadKey(true).Key;
+
+        switch (key)
+        {
+        case ConsoleKey.LeftArrow:
+        case ConsoleKey.UpArrow:
+            ClearDrawingArea();
+            cursor = (cursor - 1 + n) % n;
+            break;
+
+        case ConsoleKey.RightArrow:
+        case ConsoleKey.DownArrow:
+            ClearDrawingArea();
+            cursor = (cursor + 1) % n;
+            break;
+
+        case ConsoleKey.Escape:
+        case ConsoleKey.Backspace:
+            NextScene = new StartScene();
+            break;
+        }
+    }
+
+    private void DrawArt()
+    {
+        Card card = CollectionData.Cards[cursor];
+        if (!card.Found)
+            return;
+
+        AsciiArts.Draw(card.AsciiArt, drawPos[cursor].x, drawPos[cursor].y);
+    }
+
+    private void ClearDrawingArea()
+    {
+        int startX = 20;
+        for (int y = 0; y < 30; y++)
+        {
+            for (int x = startX; x < 120; x++)
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write(" ");
+            }
+        }
+    }
 }
